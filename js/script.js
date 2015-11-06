4c0e8f2e345983a112c764884080fba79ebbb2e5
Maria Cristina Garcia Lara <a288237@uach.mx>
2015-11-06 11:06:34 -0700

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("#prom1").click(function(){
+    $("#panel1").slideToggle("slow");
+  });
+
+  $("#prom2").click(function(){
+    $("#panel2").slideToggle("slow");
+  });
+
+  $("#prom3").click(function(){
+    $("#panel3").slideToggle("slow");
+  });
+
+  
+});
