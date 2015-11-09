3dfb26d20401b885e1284392a9c35387d451783c
Maria Cristina Garcia Lara <a288237@uach.mx>
2015-11-08 18:20:28 -0700

    commit inicial

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
   $("#prom1").click(function(){
     $("#panel1").slideToggle("slow");
+    $("#subpanel1").css("background-color","#e5eecc").css("border", "solid 1px #c3c3c3");
   });
 
   $("#prom2").click(function(){
     $("#panel2").slideToggle("slow");
+    $("#subpanel2").css("background-color","#e5eecc").css("border", "solid 1px #c3c3c3");
   });
 
   $("#prom3").click(function(){
     $("#panel3").slideToggle("slow");
+      $("#subpanel3").css("background-color","#e5eecc").css("border", "solid 1px #c3c3c3");
   });
 
-  
+
 });
